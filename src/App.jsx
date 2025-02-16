af62cde1f81836dbb6a3d7ff5539b7dd0bb67418
nileshk1901 <nileshkush1901@gmail.com>
2025-02-16 23:39:44 +0530

    video upload to server

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import VideoUpload from "./components/VideoUpload";
 function App() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 space-y-9">
-			<h1 className="text-5xl font-extrabold text-gray-100 text-center">
+			<h1 className="text-2xl font-medium text-gray-100 text-center">
 				Welcome to Video Streaming App
 			</h1>
 			{/* Wrapper for centering VideoUpload */}
--- src/components/VideoUpload.jsx
+++ src/components/VideoUpload.jsx
@@ -1,37 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import videoLogo from "../assets/upload.png";
-import { Button, Card } from "flowbite-react";
+import { Button, Card, Label, Textarea, TextInput } from "flowbite-react";
+import axios  from 'axios';
+
+
+
+
+
 
 function VideoUpload() {
+	const [selectedFile, setSelectedFile] = useState(null);
+	const [meta, setMeta] = useState({
+		title: "",
+		description:"",
+	})
+	const [progress, setProgress] = useState(0);
+	const [uploading, setUploading] = useState(false);
+	const [message, setMessage] = useState("");
+	function handleFileChange(event) {
+		console.log(event.target.files[0]);
+		setSelectedFile(event.target.files[0]);
+	}
+
+	function formFieldChange(event) {
+		// console.log(event.target.name);
+		// console.log(event.target.value);
+		setMeta({
+			...meta,
+			[event.target.name]:event.target.value,
+		})
+	}
+	function handleForm(formEvent) {
+		formEvent.preventDefault();
+		if (!selectedFile) {
+			alert("Select File");
+			return;
+		}
+		saveVideoToServer(saveVideoToServer, meta);
+		
+		
+	}
+	async function saveVideoToServer(video, videoMetaData) {
+		setUploading(true);
+		try {
+			let formData = new FormData();
+			formData.append("title", videoMetaData.title);
+			formData.append("description", videoMetaData.description);
+			formData.append("file", selectedFile);
+			let response = await axios.post(`http://localhost:8080/api/v1/videos`, formData, {
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+				onUploadProgress: (progressEvent) => {
+					console.log(progressEvent);
+				},
+			}
+			);
+			console.log(response);
+			setMessage("file uploaded");
+			
+		} catch (error) {
+			console.log(error);
+		}
+		
+	}
 	return (
 		<div >
 			<Card className="flex flex-col items-center justify-center">
-				<h1>Upload Videos</h1>
+				<h1 className="text-2xl">Upload Videos</h1>
 				<div>
-					<form className="flex  items-center space-x-6">
-						<div className="shrink-0">
-							<img
-								className="h-16 w-16 object-cover"
-								src={videoLogo}
-								alt="Upload icon"
-							/>
+					<form noValidate className="flex flex-col space-y-6" onSubmit={handleForm}>
+						<div>
+							<div className="mb-2 block">
+								<Label htmlFor="file-upload" value="Video title" />
+							</div>
+							<TextInput onChange={formFieldChange} name="title" placeholder="Enter video title" />
 						</div>
-						<label className="block">
-							<span className="sr-only">Choose video file</span>
-							<input
-								type="file"
-								className="block w-full text-sm text-gray-500
+						<div className="max-w-md">
+							<div className="mb-2 block">
+								<Label htmlFor="comment" value="Video Description" />
+							</div>
+							<Textarea onChange={ formFieldChange} name="description" placeholder="Describe your video" required rows={4} />
+						</div>
+						<div className="flex items-center space-x-5 justify-center">
+							<div className="shrink-0">
+								<img
+									className="h-16 w-16 object-cover"
+									src={videoLogo}
+									alt="Upload icon"
+								/>
+							</div>
+							<label className="block">
+								<span className="sr-only">Choose video file</span>
+								<input
+									name="file"
+									onChange={handleFileChange}
+									type="file"
+									className="block w-full text-sm text-gray-500
 								 file:mr-4 file:py-2 file:px-4
 								 file:rounded-md file:border-0
 								 file:text-sm file:font-semibold
 								 file:bg-violet-600 file:text-white
 								 hover:file:bg-violet-700 cursor-pointer"
-							/>
-						</label>
+								/>
+							</label>
+						</div>
+						<div className=" flex justify-center">
+							<Button type="submit" className="text-blue-600 border-blue-600">Submit</Button>
+
+						</div>
 					</form>
-					<Button className="w-full bg-violet-600 hover:bg-violet-700">
-						Upload
-					</Button>
+					
 				</div>
 			</Card>
 		</div>
