b94248716cb2f0b136294d8c1a31d3e02c098630
nileshk1901 <nileshkush1901@gmail.com>
2025-02-17 23:39:45 +0530

    video streaming

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import VideoUpload from "./components/VideoUpload";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+	const [videoId, setVideoId] = useState(
+		'09b83cef-b909-4807-9f08-8696c5a05a6f'
+	);
 	return (
 		<>
-			<Toaster/>
+			<Toaster />
 			<div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 space-y-9">
-				<h1 className="text-2xl font-medium text-gray-100 text-center">
+				<h1 className="text-6xl font-medium text-gray-100 text-center">
 					Welcome to Video Streaming App
 				</h1>
-				{/* Wrapper for centering VideoUpload */}
-				<div className="flex justify-center items-center w-full">
-					<VideoUpload />
+				<div className="flex mt-14 w-full justify-around ">
+					<div>
+						<h1 className="text-white">Playing Video</h1>
+						<video
+							src={`http://localhost:8080/api/v1/videos/stream/${videoId}`}
+							controls
+							autoPlay
+							muted
+							width="600"
+						/>
+					</div>
+					
+						<VideoUpload />
 				</div>
 			</div>
 		</>
